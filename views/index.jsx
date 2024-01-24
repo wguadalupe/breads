9c2d00955d00e96b4fa3f9a8bec17ddb654ab96f
wguadalupe <w.guadalupe78@gmail.com>
2024-01-23 20:37:03 -0500

    create and new routes complete

--- views/index.jsx
+++ views/index.jsx
@@ -5,7 +5,12 @@ const Default = require('./layouts/Default')
 function Index ({breads}) {
     return (
 <Default>
-  <h2>Index Page</h2>
+  <h2>Index Page
+  <div className="newButton">
+  <a href="/breads/new"><button>Add a new bread</button></a>
+</div>
+
+  </h2>
   {/* <p>I have {breads[0].name} bread!</p> */}
   {/* This is a JSX comment. */}
   <ul>
--- views/new.jsx
+++ views/new.jsx
@@ -4,7 +4,35 @@ const Default = require('./layouts/Default')
 function New () {
     return (
       <Default>
-        <h2>Add a new bread</h2>
+        <h2>Add a new bread
+        <div className="backButton">
+  <a href="/breads"><button>Go back to the index</button></a>
+</div>
+
+        </h2>
+        <form action="/breads" method="POST">
+  <label htmlFor="name">Name</label>
+  <input
+  type="text"
+  name="name"
+  id="name"
+  required 
+  />
+   <label htmlFor="image">Image</label>
+          <input
+            type="text"
+            name="image"
+            id="image"/>
+          <label htmlFor="hasGluten">Has Gluten?</label>
+          <input
+            type="checkbox"
+            name="hasGluten"
+            id="hasGluten"
+            defaultChecked
+          />
+          <br />
+          <input type="submit"/>
+        </form>
       </Default>
     )
 }
