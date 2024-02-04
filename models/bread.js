20dd2fe58bd54372afe6eabc844c6fb5563864c7
wguadalupe <w.guadalupe78@gmail.com>
2024-02-04 10:47:08 -0500

    safety push

--- models/bread.js
+++ models/bread.js
@@ -5,16 +5,17 @@ const mongoose = require('mongoose')
 // creating shorthand for the Schema constructor 
 const { Schema } = mongoose 
 
-//schema
+// schema
 const breadSchema = new Schema({
   name: { type: String, required: true },
   hasGluten: Boolean,
   image: { type: String, default: 'http://placehold.it/500x500.png' },
   baker: {
-    type: String,
-    enum: ['Rachel', 'Monica', 'Joey', 'Chandler', 'Ross', 'Phoebe']
+    type: Schema.Types.ObjectID,
+    ref: 'Baker'
   }
 })
+
 
 //helper methods
 breadSchema.methods.getBakedBy = function() {
